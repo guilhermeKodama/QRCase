712a60decd7c473d01190ef9d47fa7e1c1fffab2
Carlos Soares <carlossoares@Carlos-Soares-Mac.local>
2015-05-12 18:20:28 -0400

    Case Overview Background e imagens de botão

---
+++ DetectiveApp/CaseOverviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  CaseOverviewViewController.h
+//  QRCase
+//
+//  Created by Carlos Soares on 5/12/15.
+//  Copyright (c) 2015 Guilherme Kenji Kodama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaseOverviewViewController : UIViewController
+
+@end
---
+++ DetectiveApp/CaseOverviewViewController.m
@@ -0,0 +1,38 @@
+//
+//  CaseOverviewViewController.m
+//  QRCase
+//
+//  Created by Carlos Soares on 5/12/15.
+//  Copyright (c) 2015 Guilherme Kenji Kodama. All rights reserved.
+//
+
+#import "CaseOverviewViewController.h"
+
+@interface CaseOverviewViewController ()
+
+@end
+
+@implementation CaseOverviewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+        self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"backgroundapp"]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
